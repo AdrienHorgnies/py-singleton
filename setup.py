1b7743cfcb909c6419a2c114400768a65ccf1bce
Adrien Horgnies <adrien.pierre.horgnies@gmail.com>
2019-02-08 00:18:56 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='singletons',
+    version='0.0.1',
+    packages=['singletons',],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
