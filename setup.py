e190f833f58e30a0eb44e6a2e1be1efe90822b90
Adrien Horgnies <adrien.pierre.horgnies@gmail.com>
2019-02-08 00:30:39 +0100

    more elaborate setup

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+
+setuptools.setup(
     name='singletons',
     version='0.0.1',
-    packages=['singletons',],
-    license='MIT License',
+    author_email='adrien.pierre.horgnies@gmail.com',
+    description='Metaclass that defines a class as a singleton',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/AdrienHorgnies/py-singleton',
+    packages=setuptools.find_packages(),
+    license='MIT License',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
